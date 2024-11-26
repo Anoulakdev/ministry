adb90a11940f399802684ab4c1968b9a27530035
Anoulakdev <105956871+Anoulakdev@users.noreply.github.com>
2024-11-26 15:54:04 +0700

    26112024

---
+++ php/src/apiurl.php
@@ -0,0 +1,15 @@
+<?php
+
+$url = 'http://localhost';
+
+$apimember = $url .'/api/member.php';
+$apimemberdelete = $url .'/api/memberdelete.php';
+$apireason = $url .'/api/reason.php';
+$apincheck = $url .'/api/ncheck.php';
+$apiocheck = $url .'/api/ocheck.php';
+$apibarcode = $url .'/api/barcode.php';
+$apimembersearch = $url .'/api/membersearch.php';
+$apisheetsearch = $url .'/api/sheetsearch.php';
+$apicandidatesearch = $url .'/api/candidatesearch.php';
+
+?>
